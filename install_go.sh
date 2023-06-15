b87b990a2942c32af6df9ab8efa2109950bf4830
yash-dxt <yashdixitsq@gmail.com>
2023-06-15 16:12:10 +0530

    update go version

--- install_go.sh
+++ install_go.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Download and extract the Go binary
-curl -LO https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
-tar -C . -xzf go1.16.7.linux-amd64.tar.gz
+curl -LO https://golang.org/dl/go1.18.1.linux-amd64.tar.gz
+tar -C . -xzf go1.18.1.linux-amd64.tar.gz
 
 # Set the PATH to include the Go binary
 export PATH="$(pwd)/go/bin:$PATH"
 
 # Display the Go version
 go version
+
+go install github.com/joho/godotenv/cmd/godotenv@latest

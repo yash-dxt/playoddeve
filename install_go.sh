5c9c5ec5b3b46d79c3222312c172bfd84c9febe2
yash-dxt <yashdixitsq@gmail.com>
2023-06-15 17:04:30 +0530

    hi

--- install_go.sh
+++ install_go.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Download and extract the Go binary
-curl -LO https://golang.org/dl/go1.18.1.linux-amd64.tar.gz
-tar -C . -xzf go1.18.1.linux-amd64.tar.gz
+# curl -LO https://golang.org/dl/go1.18.1.linux-amd64.tar.gz
+# tar -C . -xzf go1.18.1.linux-amd64.tar.gz
 
-# Set the PATH to include the Go binary
-export PATH="$(pwd)/go/bin:$PATH"
+# # Set the PATH to include the Go binary
+# export PATH="$(pwd)/go/bin:$PATH"
 
 # Display the Go version
-go version
+echo "Hi"
 
